9ef736e68fe30af31f76c11a7d8afdf8dd27aa3a
mysteri0uss <mysteri0uss@outlook.com>
2018-09-04 14:57:39 +0800

    MOD: add docker.sh

---
+++ ss-libev_+_unbound-tls/docker.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install docker-ce
