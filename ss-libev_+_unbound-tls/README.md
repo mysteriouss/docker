361d22a0e0071c2a61b27073477935f88be005a1
mysteri0uss <mysteri0uss@outlook.com>
2019-01-04 18:47:40 +0800

    MOD: add reverse_proxy config

--- ss-libev_+_unbound-tls/README.md
+++ ss-libev_+_unbound-tls/README.md
@@ -3,9 +3,12 @@
 [USAGE]
 
 $ git clone 'https://github.com/mysteriouss/docker' 
-$ cd docker/ss-libev_+_unbound-tls/alpine
+$ cd docker/ss-libev_+_unbound-tls/alpine/80
+
+or using reverse proxy @ port 8989
+$ cd docker/ss-libev_+_unbound-tls/alpine/8989
 
 $ docker build -t="55-server" .
-$ docker run -it -p 80:80/tcp -p 80:80/udp 55-server
+$ docker run -it --network=host 55-server
 ```
 
--- ss-libev_+_unbound-tls/docker.sh
+++ ss-libev_+_unbound-tls/docker.sh
@@ -8,4 +8,15 @@ yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 
-yum install docker-ce
+yum install -y docker-ce
+
+cd /usr/local/src
+git clone 'https://github.com/mysteriouss/docker'
+cd docker/ss-libev_+_unbound-tls/alpine/8989
+
+docker build -t="55-server" .
+docker run -itd --network=host 55-server
+
+echo 'Server is now listening at port 8989'
+echo
+
