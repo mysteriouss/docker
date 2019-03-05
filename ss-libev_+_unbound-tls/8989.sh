4c26efe216d630f438671673857d30044c2ac4ec
mysteriouss <mysteri0uss@outlook.com>
2019-03-05 18:56:48 +0800

    MOD: update udp port

--- ss-libev_+_unbound-tls/8989.sh
+++ ss-libev_+_unbound-tls/8989.sh
@@ -3,7 +3,7 @@
 cd alpine/8989
 
 docker build -t="55-server" .
-docker run -itd --network=host 55-server
+docker run -itd -p 8989:8989/tcp -p 80:8989/udp 55-server
 
-echo 'Server is now listening at port 8989'
+echo 'Server is now listening at port tcp:8989,udp:80'
 echo
