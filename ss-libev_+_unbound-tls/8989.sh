26de8713494aa6d1bc1fc03fce353085894cfbfc
mysteriouss <mysteri0uss@outlook.com>
2019-09-29 17:16:00 +0800

    MOD: remove 8989.sh

--- ss-libev_+_unbound-tls/8989.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd alpine/8989
-
-docker build -t="55-server" .
-docker run -itd -p 8989:8989/tcp -p 80:8989/udp 55-server
-
-echo 'Server is now listening at port tcp:8989,udp:80'
-echo
