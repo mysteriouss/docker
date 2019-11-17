17073a5320da264b7168a0e3f1d28d2535fa9482
mysteriouss <mysteri0uss@outlook.com>
2019-11-17 15:38:55 +0800

    MOD: fix 80.sh

--- ss-libev_+_unbound-tls/80.sh
+++ ss-libev_+_unbound-tls/80.sh
@@ -2,10 +2,10 @@
 
 cd alpine/80
 
-docker container ls | grep 55-server | awk '{printf $1}' | xargs docker stop 
+docker container ls | grep 55-server | awk '{printf $1}' | xargs docker stop
 docker container prune -f
 docker build -t="55-server" .
-docker run -itd --network=host 55-server -v config:/config:rw -v service:/service:rw
+docker run -itd --network=host -v $(pwd)/config:/config:rw 55-server
 
 echo 'Server is now listening at port 80'
 echo
