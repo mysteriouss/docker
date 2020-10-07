c77fd1ec1dc1a750dd4b4902d83cebee1bd244f3
mysteriouss <mysteri0uss@outlook.com>
2020-10-07 18:46:00 +0800

    MOD: bump to v3.3.5

--- install.sh
+++ install.sh
@@ -1,15 +1,53 @@
 #!/bin/sh
 
-yum install -y yum-utils \
-  device-mapper-persistent-data \
-  lvm2
+_red() {
+    printf '\033[1;31;31m%b\033[0m' "$1"
+}
 
-yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
+_os_full() {
+    [ -f /etc/os-release ] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release && return
+}
 
-yum install -y epel-release docker-ce docker-compose
+_error() {
+    _red "[Error] "
+    printf -- "%s" "$1"
+    printf "\n"
+    exit 1
+}
 
-systemctl start docker
-systemctl enable docker
+_centos() {
+    yum install -y yum-utils \
+      device-mapper-persistent-data \
+      lvm2
 
+    yum-config-manager \
+        --add-repo \
+        https://download.docker.com/linux/centos/docker-ce.repo
+
+    yum install -y epel-release docker-ce docker-compose
+
+    systemctl start docker
+    systemctl enable docker
+}
+
+_arch() {
+    pacman -S docker docker-compose
+    systemctl start docker
+    systemctl enable docker
+}
+
+_main() {
+    case "$(_os_full)" in
+        "Arch Linux")
+            _arch
+            ;;
+        "CentOS Linux 7")
+            _centos
+            ;;
+        *)
+            _error "Not supported OS"
+            ;;
+    esac
+}
+
+_main
--- ss-libev/README.md
+++ ss-libev/README.md
@@ -1,9 +1,23 @@
 
 ```bash
 
+  #CentOS 7
+
   p=YOUR_CUSTOM_PASSWORD && \
 
   yum update -y && yum install -y git && \
+  git clone 'https://github.com/mysteriouss/docker.git' && \
+  sh docker/install.sh && \
+  cd docker/ss-libev/alpine && \
+  sed 's/YOURPASSWORD/'$p'/g' -i config/55-server/config.json && \
+  sed 's/YOURPASSWORD/'$p'/g' -i config/55-server-8989/config.json && \
+  docker-compose up
+
+  #Arch
+
+  p=YOUR_CUSTOM_PASSWORD && \
+
+  yes | pacman -Sy git && \
   git clone 'https://github.com/mysteriouss/docker.git' && \
   sh docker/install.sh && \
   cd docker/ss-libev/alpine && \
